006e9241b41ed4ae0a00511c4895450fb43e1924
Blake McBride <blake@mcbridemail.com>
2024-04-20 17:21:59 -0500

    Update

--- src/main/application/KissInit.groovy
+++ src/main/application/KissInit.groovy
@@ -11,11 +11,11 @@ class KissInit {
         MainServlet.setUser "postgres"            // database user (not application user login)
         MainServlet.setPassword "postgres"        // database password (not application user password)
         MainServlet.setMaxWorkerThreads 40        // max number of simultaneous REST services (any additional are put on a queue)
-        MainServlet.setAmazonApplicationId "amzn1.ask.skill.806640da-2a09-43cb-b1da-bac959fe3606"
+        MainServlet.setAmazonApplicationId "amzn1.ask.skill.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
         MainServlet.setSmtpHost "email-smtp.us-east-1.amazonaws.com"
-        MainServlet.setSmtpUsername "AKIAJADABYZ7WHXJFXTQ"
-        MainServlet.setSmtpPassword "AhNs13iGI525G2f4lulMFOaNUkYP90238CaDhBOqkv64"
+        MainServlet.setSmtpUsername "xxxxxxxxxxxxxxxxxxxx"
+        MainServlet.setSmtpPassword "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
         MainServlet.setDebug true                // if true print debug info
     }
